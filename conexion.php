a7da01d82cce7dffb7c675c8489e798414e4026b
DamianHB <is.demian27@gmail.com>
2026-05-24 20:55:17 -0400

    Conectar con Railway PostgreSQL

--- conexion.php
+++ conexion.php
@@ -1,15 +1,15 @@
 <?php
-$host      = "postgres.railway.internal";
-$puerto    = "5432";
-$basedatos = "railway";
-$usuario   = "postgres";
-$password  = "YOqYsUzTdXJMqrTZbextWWHntzgBFptc";
+$host      = "postgres.zephyr.proxy.rlwy.net";
+$puerto    = "47530";
+$basedatos = "Ferrocarril";
+$usuario   = "Postgres";
+$password  = "YOqYsUzTdXMMqrTZbextWHntzgBptc";
 
 $conexion = pg_connect("host=$host port=$puerto dbname=$basedatos user=$usuario password=$password");
 
 if (!$conexion) {
     die("❌ Error al conectar con la base de datos");
 } else {
-    echo "✅ Conexión exitosa a PostgreSQL";
+    echo "✅ Conexión exitosa a PostgreSQL Railway";
 }
 ?>

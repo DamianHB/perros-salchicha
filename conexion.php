673537efbc993331f3be4a4e59aaff0d1e8c2451
DamianHB <is.demian27@gmail.com>
2026-05-25 13:03:37 -0400

    Fix contraseña PostgreSQL correcta

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@ $host      = getenv('PGHOST')     ?: 'postgres.railway.internal';
 $puerto    = getenv('PGPORT')     ?: '5432';
 $basedatos = getenv('PGDATABASE') ?: 'Ferrocarril';
 $usuario   = getenv('PGUSER')     ?: 'postgres';
-$password  = getenv('PGPASSWORD') ?: 'YOqYsUzTdXMMqrTZbextWHntzgBptc';
+$password  = getenv('PGPASSWORD') ?: 'YOqYsUzTdXJMqrTZbextWWHntzgBFptc';
 
 $conexion = pg_connect("host=$host port=$puerto dbname=$basedatos user=$usuario password=$password");
 
